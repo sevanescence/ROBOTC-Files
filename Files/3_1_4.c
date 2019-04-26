475dec22c093579a448f3247736accd8b15853e6
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-04-26 10:27:59 -0500

    Create 3_1_4.c

---
+++ Files/3_1_4.c
@@ -0,0 +1,80 @@
+void callLightSensorForFlashlight() {
+
+  while(true) {
+  
+    if (SensorValue(lightSensor) > 700) {
+      // SensorValue is the function used for measuring
+      // the values of the input sensor. Writing
+      // (lightSensor > 700) will not work. lightSensor
+      // is not a value, it's an object.
+    
+      turnFlashlightOn(flashlight, 127);
+    
+    } else {
+    
+      turnFlashlightOff(flashlight);
+    
+    }
+  
+  }
+
+}
+
+void checkLimitSwitchAndLightSensorForFlashligh() {
+
+  while(true) {
+  
+    while(SensorValue(limitSwitch) != 0) {
+    
+      if (SensorValue(lightSensor > 700) {
+      
+        turnFlashlightOn(flashlight, 127);
+      
+      } else {
+      
+        turnFlashlightOff(flashlight);
+      
+      }
+    
+    }
+  
+  }
+
+}
+
+void toggleLEDTimer() {
+
+  ClearTImer(T1); // initializes the timer used below.
+  
+  while (time1(T1) < 20000) { // 20000 milliseconds = 20 seconds.
+    // the timer will count as time passes, and will stop at
+    // 20000 milliseconds.
+  
+    turnLEDOn(green);
+    
+    wait(2);
+    
+    turnLEDOff(green);
+    
+    wait(2);
+  
+  }
+
+}
+
+task main() {
+
+  callLightSensorForFlashlight(); // executes "void callLighSensorForFlashlight()" defined above.
+  // writing your code neatly like this makes it
+  // a lot easier to disable entire sections of your
+  // code to test other parts of your project.
+  
+  checkLimitSwitchAndLightSensorForFlashlight();
+  
+  // careful! don't forget that everything is case-sensitive.
+  
+  toggleLEDTimer();
+  
+  // try part 4 on your own. if im in your class, ask me anything.
+
+}
