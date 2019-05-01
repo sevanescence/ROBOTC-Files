1d5b1f626bac41aa1c92263097fdf32ca72b6d96
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-01 15:37:50 -0500

    Update timeServoInDirection.c

--- Files/timeServoInDirection.c
+++ Files/timeServoInDirection.c
@@ -1,5 +1,39 @@
-void timeServoInDirection(int time, String direction) {
-
+void timeServoPositive(int time) {
   
+  if (SensorValue(servoMotor) >= 127) {
+    return;
+  }
+  
+  clearTimer(T1);
+  
+  while (time1(T1) < (time * 1000)) {
+    
+    if (SensorValue(servoMotor) >= 127) {
+      break;
+    }
+    
+    setServo(servoMotor, SensorValue(servoMotor) + 1);
+    
+  }
+  
+}
 
+void timeServoNegative(int time) {
+  
+  if (SensorValue(servoMotor) <= -127) {
+    return;
+  }
+  
+  clearTimer(T1);
+  
+  while (time1(T1) < (time * 1000)) {
+    
+    if (SensorValue(servoMotor) <= -127) {
+      break;
+    }
+    
+    setServo(servoMotor, SensorValue(servoMotor) - 1);
+    
+  }
+  
 }
