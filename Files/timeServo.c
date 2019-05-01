711eb9b6b6805dac757c4948868667bd570fa14a
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-01 12:04:03 -0500

    Fixed

--- Files/timeServo.c
+++ Files/timeServo.c
@@ -1,37 +1,37 @@
 void timeServo(float time) {
   
-  bool temp = !(SensorValue(servoMotor) > 126);
-
+  bool temp = (SensorValue(servoMotor) > -127);
+  
   clearTimer(T1);
   
   while (time1(T1) < (time * 1000)) {
-  
-    if (temp) {
     
-      if (SensorValue(servoMotor) < -126) {
+    if (temp) {
       
+      if (SensorValue(servoMotor) == 127) {
+        
         temp = !temp;
         
         continue;
-      
+        
       }
       
       setServo(servoMotor, SensorValue(servoMotor) + 1);
-    
-    } else {
-    
-      if (SensorValue(servoMotor) > 126) {
       
+    } else {
+      
+      if (SensorValue(servoMotor) == -127) {
+        
         temp = !temp;
         
         continue;
-      
+        
       }
       
       setServo(servoMotor, SensorValue(servoMotor) - 1);
-    
+      
     }
-  
+    
   }
-
+  
 }
