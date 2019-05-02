47f4b19e0fe74e9dd81ce55a2054364968d5c5d8
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-02 09:30:11 -0500

    Better startServo
    startServo([Positive|Negative])(motorIndex, double speed, bool loop, float time);
    
    startServoPositive() - Move the servo motor in the positive direction (towards 127)
    startServoNegative() - Move the servo motor in the negative direction (towards -127)
    
    motorIndex - the name of your servo motor (i.e. servoMotor)
    speed - the rate at which your servo motor moves (0.1 - 127, will bug out as it approaches 127. 0.5 - 1 is a good speed)
    loop - whether or not you want the servo motor to reset its position to the opposite direction (true, false)
    time - number of seconds your servo motor will turn
    
    startServoPositive(servoMotor, 1, false, 5); - servo will run for 5 seconds OR until the motor value is 127.
    startServoPositive(servoMotor, 1, true, 20); - servo will run for 20 seconds, and will continue resetting the position during that duration.

---
+++ Files/startServo.c
@@ -0,0 +1,66 @@
+void startServoPositive(tMotor motorIndex, double speed, bool loop, float time) {
+
+  bool temp = true;
+
+  if (SensorValue(motorIndex) >= 127) {
+  
+    if (!checkServoCondition(motorIndex, loop, temp)) return;
+  
+  }
+  
+  clearTimer(T1);
+  
+  while (time1(T1) < (time * 1000)) {
+  
+    if (SensorValue(motorIndex) >= 127) {
+    
+      if (!checkServoCondition(motorIndex, loop, temp)) break;
+    
+    }
+    
+    setServo(motorIndex, SensorValue(motorIndex) + speed);
+  
+  }
+
+}
+
+void startServoNegative(tMotor motorIndex, double speed, bool loop, float time) {
+
+  bool temp = false;
+  
+  if (SensorValue(motorIndex) <= -127) {
+  
+    if (!checkServoCondition(motorIndex, loop, temp)) return;
+  
+  }
+  
+  clearTimer(T1);
+  
+  while (time1(T1) < (time * 1000)) {
+  
+    if (SensorValue(motorIndex) <= -127) {
+    
+      if (!checkMotorCondition(motorIndex, loop, temp)) break;
+    
+    }
+    
+    setServo(motorIndex, SensorValue(motorIndex) - speed);
+  
+  }
+
+}
+
+bool checkServoCondition(tMotor motorIndex, bool loop, bool direction) {
+
+  if (loop) {
+  
+    if (direction) setServo(motorIndex, -127);
+      else setServo(motorIndex, 127);
+    
+    return true;
+  
+  }
+  
+  return false;
+
+}
